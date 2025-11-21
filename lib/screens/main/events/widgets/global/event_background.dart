e9f5a9d229c4668b5a551a42c3ab6b9f3d3fe1c9
MehmetAkan <akan.mehmet2700@gmail.com>
2025-11-21 17:00:49 +0300

    animation problem

--- lib/screens/main/events/widgets/global/event_background.dart
+++ lib/screens/main/events/widgets/global/event_background.dart
@@ -6,42 +6,43 @@ import 'package:lobi_application/widgets/common/images/app_image.dart';
 class EventBackground extends StatelessWidget {
   final String? coverPhotoUrl;
 
-
   final String? defaultCoverAsset;
 
   const EventBackground({
     super.key,
     this.coverPhotoUrl,
-    this.defaultCoverAsset, 
+    this.defaultCoverAsset,
   });
 
-@override
-Widget build(BuildContext context) {
-  return Positioned.fill(
-    child: Stack(
-      children: [
-        Positioned.fill(
-          child: Transform.scale(
-            scale: 1.4,
-            child: AppImage(
-              path: coverPhotoUrl,
-              fallbackPath:
-                  defaultCoverAsset ?? 'assets/images/system/event-example-black.png',
-              fit: BoxFit.cover,
+  @override
+  Widget build(BuildContext context) {
+    return Positioned.fill(
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: ClipRect(
+              child: Transform.scale(
+                scale: 1.4,
+                child: AppImage(
+                  path: coverPhotoUrl,
+                  fallbackPath:
+                      defaultCoverAsset ??
+                      'assets/images/system/event-example-black.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
             ),
           ),
-        ),
-        ClipRect(
-          child: BackdropFilter(
-            filter: ImageFilter.blur(sigmaX: 120, sigmaY: 120),
-            child: Container(
-              color: AppTheme.getCreateEventBg(context).withOpacity(0.40),
+          ClipRect(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 120, sigmaY: 120),
+              child: Container(
+                color: AppTheme.getCreateEventBg(context).withOpacity(0.40),
+              ),
             ),
           ),
-        ),
-      ],
-    ),
-  );
-}
-
+        ],
+      ),
+    );
+  }
 }
